52bcbc68ed6b18099bd423df058c2081e3df1a96
Никита <Ployka322@yandex.ru>
2022-12-12 14:28:23 +0700

    homeWork done

--- src/Main.java
+++ src/Main.java
@@ -44,7 +44,7 @@ public class Main {
 
     private static void task3() {
         System.out.println("Задача 3");
-        int year = 2022;
+        int year = 2023;
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("Год" + year + "  - високостный");
         } else {
